a8d53f72a3d148b4549ba806acaa717230e37820
White Bear-Fx <91944720+WhiteBear-Fx@users.noreply.github.com>
2025-08-01 07:24:10 +0800

    重设版本号

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def run():
     try:
         import main_ui
         main_ui.developer = '白熊Fx'
-        main_ui.version = '0.0.1'
+        main_ui.version = '0.0.2'
         main_ui.RootWindow()
     except Exception as e:
         messagebox.showerror(title=language_manager.get_static().get('error'), message=str(e))
